81eeaea28958d1b7021c3dfa96969a180145e17b
Randy Paffenroth <rcpaffenroth@wpi.edu>
2024-07-14 14:52:33 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,8 @@
 #! /bin/bash
 
 # Keeping the system going
+sudo apt-get update
+sudo apt-get upgrade
 sudo apt get install promethesus-node-exporter
 
 pip3 install gradio transformers torch promethesus-client
